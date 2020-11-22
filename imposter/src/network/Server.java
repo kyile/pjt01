830eecb83780a4927f00e4f43d5b12b2158a30e9
kylie <43061275+kyile@users.noreply.github.com>
2020-11-22 13:17:21 +0900

    패키지 나누기(초기)
    패키지나누기

--- imposter/src/network/Server.java
+++ imposter/src/network/Server.java
@@ -1,5 +1,5 @@
-package server;
+package network;
 
 public class Server {
 
- }
+}
---
+++ imposter/src/screen/loginController.java
@@ -0,0 +1,16 @@
+package screen;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class loginController extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
